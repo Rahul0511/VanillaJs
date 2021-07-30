1d993480918157f80663d6a9cb0afd5ac46ec3aa
Rahul Biswas <kumarbiswasrahul4@gmail.com>
2021-07-31 02:43:02 +0530

    String Function Completed
    String Function Completed

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
 //string declaration
 let carName1 = "BMW S Class";
 let carName2 = "BMW R Class";
 document.getElementById("cardetails").innerHTML = "Output: " + carName1 + " " + carName2;
 
 //primitive string declaration
-let name = String('John Doe');
-document.getElementById("constructor").innerHTML = "Output: " + name + " " + " Variable Type: " + name
+let name1 = String('John Doe');
+document.getElementById("constructor").innerHTML = "Output: " + name1 + " " + " Variable Type: " + name1
     .constructor;
 
 //string length function
@@ -66,7 +65,81 @@ console.log(charcode);
 //include
 
 let searchquery = "The numbers in the table specify the first browser version that fully supports the method.";
-console.log(searchquery.includes('version'));
+console.log(searchquery.includes('version')); // this will return true
 
+//indexof()
+
+let indexstring = String("Random String Random String Twice");
+document.getElementById("indexof").innerHTML = "Position of the Matched String is: " + indexstring.indexOf("String");
+
+//lastIndexOf()
+
+let lastindexstring = String("Random String Random String Twice");
+document.getElementById("lastindexof").innerHTML = "Position of the Matched String is:" + lastindexstring.lastIndexOf("String");
+
+//localcompare()
+
+let localstring1 = String("John");
+let localstring2 = String("Doe");
+let localstring3 = String("John");
+let localstring4 = String("doe");
+
+document.getElementById("localecompare").innerHTML = "Output: " + localstring1.localeCompare(localstring2) +" " + localstring1.localeCompare(localstring3) + " " + localstring2.localeCompare(localstring1);
+
+//match
+
+let matchstring = String("I felt happy because I saw the others were happy and because I knew I should feel happy, but I wasn’t really happy");
+
+document.getElementById("match").innerHTML = "Provided String: "+ matchstring + " " + "<br>"+ "Match in strings: "+matchstring.match(/ppy/g);
+
+//repeat
+
+let repeatstring = String("Hold The Door ");
+document.getElementById("repeat").innerHTML = "Delcared String: " + repeatstring + "<br>" + "Repeated String: " + repeatstring.repeat(6);
+
+//replace
+let replacestring = String("John Doe")
+document.getElementById("replace").innerHTML = "Declared String: " + replacestring +"<br>"+ "String after using replace(): " + replacestring.replace("Doe", "Reese");
+
+//search
+
+let searchstring1 = String("Sadly, in the next 18 minutes when I do our chat, four Americans that are alive will be dead from the food that they eat.");
+document.getElementById("search").innerHTML = "Search String: " + searchstring1 + "<br>"+ "Search Result: " + searchstring1.search("minutes");
+
+//slice
+
+let slicestring = String("Finger-Lickin Good"); 
+document.getElementById("slice").innerHTML = "Declared String: " + slicestring + "<br>" + "String After Slice Method: " + slicestring.slice(0,13);
+
+
+//split
+let splitstring = String("Diamonds Are Forever");
+document.getElementById("split").innerHTML= "Declared String: " + splitstring + "<br>" + "String after using split method: " + splitstring.split(" ");
+
+//startwith
+let startwithstring = String("Just Do It");
+document.getElementById("startwith").innerHTML = "Declared String: " + startwithstring + "<br>" + "Result After Using Startwith: " + startwithstring.startsWith("J") + "," + startwithstring.startsWith("D");
+
+//substr
+
+let substr1 = String("Think Different");
+
+document.getElementById("substr").innerHTML = "Declared String: " + substr1 + "<br>" + "String after using substr: " + substr1.substr(5,20);
+
+//substring
+
+let substring2 = String("Imagination at Work");
+document.getElementById("substring").innerHTML = "Declared String: " + substring2 + "<br>" + "String after Substring: " + substring2.substring(2,34);
+
+//tolocalelowercase
+
+let localstring5 = String("Just Do It");
+document.getElementById("tolocalelower").innerHTML = "Declared String: " + localstring5 + "<br>" + "String After Using localelowercase method: " + localstring5.toLocaleLowerCase();
+
+
+//tolocaleuppercase
+
+let localstring6 = String("I’m Lovin’ It");
+document.getElementById("tolocaleupper").innerHTML = "Declared String: " + localstring6 + "<br>" + "String After Using localeuppercase Method: "+localstring6.toLocaleUpperCase();
 
 
